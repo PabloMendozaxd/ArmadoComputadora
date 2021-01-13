2c20c8529a65d9a9c0d9ebbec52a0d40e22ceb54
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-01-13 00:12:41 -0600

    clase para el manejo del objeto documento

--- armado.js
+++ armado.js
@@ -2,7 +2,7 @@
 // http://demo6292426.mockable.io/processors
 "use strict";
 class Data {
-  constructor() {}//ES necesario?
+  constructor() {} //ES necesario?
   requestData(url, defaultDAta) {
     return new Promise((resolve) => {
       let myRequest = new XMLHttpRequest();
@@ -20,3 +20,38 @@ class Data {
   }
 }
 
+class DOM {
+  static createNode(tag) {
+    return document.createElement(tag);
+  }
+  static addEventListener(element, nameEvento, handler, context) {
+    element.addEventListener(nameEvento, handler);
+  }
+  static appendChild(mainNode, node) {
+    mainNode.appendChild(node);
+  }
+  static setAttribute(node, nameAttribute, value) {
+    node.setAttribute(nameAttribute, value);
+  }
+  static setTextContent(node, textContent) {
+    node.textContent = textContent;
+  }
+}
+
+class PC{
+  body
+  request
+  ramSelected
+  processorSelected
+  processors
+  rams 
+  constructor(){
+    this.body=document.body
+    this.request=new Data()
+    this.ramSelected=undefined;
+    this.processorSelected=undefined;
+  }
+  inicia(){
+    const div = //metodo para crear div
+  }
+}
