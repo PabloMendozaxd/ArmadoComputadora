24ae7f8ccb20335dfdf6521102bc7d9af51acc08
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-01-12 23:24:44 -0600

    request con manejador de error 404

--- armado.js
+++ armado.js
@@ -1,9 +1,22 @@
 // http://demo6292426.mockable.io/ramMemory
 // http://demo6292426.mockable.io/processors
-'use strict'
+"use strict";
 class Data {
-  constructor(){
-
+  constructor() {}//ES necesario?
+  requestData(url, defaultDAta) {
+    return new Promise((resolve) => {
+      let myRequest = new XMLHttpRequest();
+      myRequest.addEventListener("load", function () {
+        //meter un switch para fresear
+        if (status === 404) {
+          console.error("Ha ocurrido un error :( ");
+        } else {
+          resolve(JSON.parse(this.responseText).data);
+        }
+      });
+      myRequest.open("GET", url);
+      myRequest.send();
+    });
   }
-  requestData
 }
+
